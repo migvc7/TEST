460aa04da8233c14f9ce90b8b344808ef7b00266
AIS <migvc7@gmail.com>
2024-02-08 00:34:30 +0800

    asdfasdf

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,4 +1,4 @@
-import { onCleanup, onMount } from 'solid-js';
+import { onMount } from 'solid-js';
 
 type Props = {
   botContainer: HTMLDivElement | undefined;
@@ -10,31 +10,12 @@ const defaultTextColor = '#303235';
 
 export const Badge = (props: Props) => {
   let liteBadge: HTMLAnchorElement | undefined;
-  let observer: MutationObserver | undefined;
-
-  const appendBadgeIfNecessary = (mutations: MutationRecord[]) => {
-    mutations.forEach((mutation) => {
-      mutation.removedNodes.forEach((removedNode) => {
-        if ('id' in removedNode && liteBadge && removedNode.id == 'lite-badge') {
-          console.log("Sorry, you can't remove the brand 😅");
-          props.botContainer?.append(liteBadge);
-        }
-      });
-    });
-  };
 
   onMount(() => {
-    if (!document || !props.botContainer) return;
-    observer = new MutationObserver(appendBadgeIfNecessary);
-    observer.observe(props.botContainer, {
-      subtree: false,
-      childList: true,
-    });
+    // Any necessary initialization when the component mounts, if required
   });
 
-  onCleanup(() => {
-    if (observer) observer.disconnect();
-  });
+  // Removed onCleanup with observer logic since we are no longer monitoring changes to revert them
 
   return (
     <span
